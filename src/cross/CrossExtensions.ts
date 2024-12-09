7a5f10ba9ed079f885a7953881c4c6e0311152cd
kindabrazy <kindofbrazy@gmail.com>
2024-12-09 13:20:02 +0330

    chore(types): add cross types file

---
+++ src/cross/CrossExtensions.ts
@@ -0,0 +1,32 @@
+export type PythonInstallation = {
+  version: string;
+  installationType: 'official' | 'conda' | 'other';
+  architecture: '32bit' | '64bit';
+  installPath: string;
+  condaName: string;
+  installFolder: string;
+  pipPath?: string;
+  venvPaths: string[];
+  sitePackagesPath: string;
+  isDefault: boolean;
+};
+
+export type UninstallResult = {
+  success: boolean;
+  message: string;
+};
+
+export type PythonVersion = {
+  version: string;
+  url: string;
+};
+
+export const pythonChannels = {
+  getAvailableConda: 'get-available-conda-pythons',
+  downloadProgressConda: 'download-conda-python-progress',
+  installConda: 'install-conda-python',
+
+  getAvailableOfficial: 'get-available-pythons',
+  downloadProgressOfficial: 'download-python-progress',
+  installOfficial: 'install-official-python',
+};
