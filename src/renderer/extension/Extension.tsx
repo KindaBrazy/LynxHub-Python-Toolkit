49da5735036185a8f64d0a9092c4a6f58cb14d11
kindabrazy <kindofbrazy@gmail.com>
2024-12-10 18:18:34 +0330

    style(icons): replace python icon with colored version

--- src/renderer/extension/Extension.tsx
+++ src/renderer/extension/Extension.tsx
@@ -37,7 +37,7 @@ function ToolsPage() {
         className="w-60 h-auto py-4 shadow-small hover:shadow-medium transition-shadow duration-300">
         <div className="space-y-4 size-full">
           <span className="justify-center flex font-semibold text-[13pt]">Python Toolkit</span>
-          <Python_Icon className="size-full p-4 opacity-70" />
+          <Python_Icon className="size-full p-5" />
         </div>
       </Button>
       <PythonToolkitModal isOpen={isOpen} setIsOpen={setIsOpen} />
