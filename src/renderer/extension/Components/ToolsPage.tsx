b745c02b8497ba94b78cbf17e6f08ec0fd801953
kindabrazy <kindofbrazy@gmail.com>
2024-12-29 14:36:57 +0330

    feat(ToolsPage): replace Python Toolkit button with a card component for enhanced UI and interaction

--- src/renderer/extension/Components/ToolsPage.tsx
+++ src/renderer/extension/Components/ToolsPage.tsx
@@ -1,6 +1,7 @@
-import {Button} from '@nextui-org/react';
+import {Button, Card, CardBody, CardHeader} from '@nextui-org/react';
 import {useState} from 'react';
 
+import {Play_Icon} from '../../src/assets/icons/SvgIcons/SvgIcons2';
 import PythonToolkitModal from './Python/PythonToolkitModal';
 import {Python_Color_Icon} from './SvgIcons';
 import UIProvider from './UIProvider';
@@ -14,15 +15,21 @@ export default function ToolsPage() {
 
   return (
     <UIProvider>
-      <Button
-        variant="faded"
-        onPress={openModal}
-        className="w-60 h-auto py-4 shadow-small hover:shadow-medium transition-shadow duration-300">
-        <div className="space-y-4 size-full">
-          <span className="justify-center flex font-semibold text-[13pt]">Python Toolkit</span>
-          <Python_Color_Icon className="size-full p-5" />
-        </div>
-      </Button>
+      <Card
+        className={
+          `w-[230px] h-fit cursor-default shadow-md !transition ` +
+          ` border-1 border-foreground/10 duration-300 hover:shadow-xl dark:bg-[#3d3d3d]`
+        }>
+        <CardHeader>
+          <Python_Color_Icon className="w-full h-32" />
+        </CardHeader>
+        <CardBody className="text-center text-lg font-bold">Python Toolkit</CardBody>
+        <CardBody>
+          <Button color="primary" onPress={openModal} fullWidth>
+            <Play_Icon className="size-4" />
+          </Button>
+        </CardBody>
+      </Card>
       <PythonToolkitModal isOpen={isOpen} setIsOpen={setIsOpen} />
     </UIProvider>
   );
