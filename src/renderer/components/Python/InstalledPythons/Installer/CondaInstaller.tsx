e093b09ca7a45509cfaf145eb3ae857d9e7545be
kindabrazy <kindofbrazy@gmail.com>
2025-08-07 20:39:18 +0330

    feat(installer_conda): enhance error handling and improve Python version fetching mechanism

--- src/renderer/components/Python/InstalledPythons/Installer/CondaInstaller.tsx
+++ src/renderer/components/Python/InstalledPythons/Installer/CondaInstaller.tsx
@@ -10,13 +10,14 @@ import {
   Spinner,
 } from '@heroui/react';
 import {List, Result, Tooltip} from 'antd';
-import {isEmpty, isNil} from 'lodash';
+import {isEmpty, isNil, isString} from 'lodash';
 import {OverlayScrollbarsComponent} from 'overlayscrollbars-react';
 import {Dispatch, SetStateAction, useEffect, useState} from 'react';
 
 import {useAppState} from '../../../../../../../src/renderer/src/App/Redux/Reducer/AppReducer';
 import {Circle_Icon, Refresh_Icon} from '../../../../../../../src/renderer/src/assets/icons/SvgIcons/SvgIcons';
 import pIpc from '../../../../PIpc';
+import {Warn_Icon} from '../../../SvgIcons';
 
 const CACHE_KEY = 'available-conda-pythons-list';
 
@@ -40,6 +41,12 @@ export default function InstallerConda({refresh, installed, closeModal, isOpen, 
   const [percentage, setPercentage] = useState<number>(0);
   const [envName, setEnvName] = useState<string>('');
   const [isCondaInstalled, setIsCondaInstalled] = useState<boolean | undefined>(undefined);
+  const [errorLoadingVersion, setErrorLoadingVersion] = useState<{title: string; description: string} | undefined>({
+    title: 'Failed to fetch available Python versions',
+    description:
+      'Please check your internet connection and try again.' +
+      ' If the problem persists, please open an issue on GitHub.',
+  });
 
   useEffect(() => {
     pIpc.isCondaInstalled().then(result => {
@@ -60,18 +67,49 @@ export default function InstallerConda({refresh, installed, closeModal, isOpen, 
   }, [inputValue, versions]);
 
   const fetchPythonList = (refresh: boolean) => {
+    setErrorLoadingVersion(undefined);
+
+    const getFreshData = () => {
+      pIpc
+        .getAvailableConda()
+        .then((result: string[]) => {
+          console.log('result: ', result);
+          localStorage.setItem(CACHE_KEY, JSON.stringify(result));
+          const filteredVersions = result.filter(item => !installed.includes(item));
+          setVersions(filteredVersions);
+          setLoadingList(false);
+          setErrorLoadingVersion(undefined);
+        })
+        .catch(e => {
+          console.log('error: ', e);
+          if (e.message && isString(e.message)) {
+            setErrorLoadingVersion({title: 'Failed to fetch available Python versions', description: e.message});
+          } else {
+            setErrorLoadingVersion({
+              title: 'Failed to fetch available Python versions',
+              description:
+                'Please check your internet connection and validate conda.' +
+                ' If the problem persists, please open an issue on GitHub.',
+            });
+          }
+        });
+    };
+
     setLoadingList(true);
+
     const cachedList = localStorage.getItem(CACHE_KEY);
+
     if (!refresh && !isNil(cachedList)) {
-      setVersions((JSON.parse(cachedList) as string[]).filter(item => !installed.includes(item)));
-      setLoadingList(false);
-    } else {
-      pIpc.getAvailableConda().then((result: string[]) => {
-        localStorage.setItem(CACHE_KEY, JSON.stringify(result));
-        const filteredVersions = result.filter(item => !installed.includes(item));
-        setVersions(filteredVersions);
+      const cachedVersions = (JSON.parse(cachedList) as string[]).filter(item => !installed.includes(item));
+
+      if (isEmpty(cachedVersions)) {
+        getFreshData();
+      } else {
+        setVersions(cachedVersions);
         setLoadingList(false);
-      });
+      }
+    } else {
+      getFreshData();
     }
   };
 
@@ -147,72 +185,80 @@ export default function InstallerConda({refresh, installed, closeModal, isOpen, 
           </Tooltip>
         </div>
       )}
-      <OverlayScrollbarsComponent
-        options={{
-          overflow: {x: 'hidden', y: 'scroll'},
-          scrollbars: {
-            autoHide: 'move',
-            clickScroll: true,
-            theme: isDarkMode ? 'os-theme-light' : 'os-theme-dark',
-          },
-        }}
-        className={`pr-3 mr-1 pl-4 pb-4 text-center`}>
-        {!isEmpty(installingVersion) ? (
-          <Progress
-            value={percentage}
-            className="my-4 px-2"
-            isIndeterminate={percentage === 0 || percentage === 100}
-            label={`Installing Conda Python v${installingVersion}...`}
-            showValueLabel
-          />
-        ) : loadingList ? (
-          <Spinner
-            size="lg"
-            className="justify-self-center my-4"
-            label="Loading available Conda Python versions..."
-            classNames={{circle2: 'border-b-[#ffe66e]', circle1: 'border-b-[#ffe66e] '}}
-          />
-        ) : (
-          <List
-            renderItem={item => (
-              <List.Item
-                actions={[
-                  <Popover key={'install_conda_python'}>
-                    <PopoverTrigger>
-                      <Button size="sm" variant="flat">
-                        Install
-                      </Button>
-                    </PopoverTrigger>
-                    <PopoverContent>
-                      <div className="px-2 py-1 space-y-2 text-center">
-                        <Input
-                          onKeyUp={(event: any) => {
-                            if (event.key === 'Enter') {
-                              installPython(item);
-                            }
-                          }}
-                          size="sm"
-                          value={envName}
-                          onValueChange={setEnvName}
-                          placeholder="Enter Conda environment name..."
-                        />
-                        <Button size="sm" onPress={() => installPython(item)} fullWidth>
-                          Install v{item}
+      {errorLoadingVersion ? (
+        <div className="size-full py-2 px-16 text-danger flex flex-col items-center justify-center gap-4">
+          <Warn_Icon className="size-20" />
+          <span className="text-lg">{errorLoadingVersion.title}</span>
+          <span className="text-warning/50 text-sm">{errorLoadingVersion.description}</span>
+        </div>
+      ) : (
+        <OverlayScrollbarsComponent
+          options={{
+            overflow: {x: 'hidden', y: 'scroll'},
+            scrollbars: {
+              autoHide: 'move',
+              clickScroll: true,
+              theme: isDarkMode ? 'os-theme-light' : 'os-theme-dark',
+            },
+          }}
+          className={`pr-3 mr-1 pl-4 pb-4 text-center`}>
+          {!isEmpty(installingVersion) ? (
+            <Progress
+              value={percentage}
+              className="my-4 px-2"
+              isIndeterminate={percentage === 0 || percentage === 100}
+              label={`Installing Conda Python v${installingVersion}...`}
+              showValueLabel
+            />
+          ) : loadingList ? (
+            <Spinner
+              size="lg"
+              className="justify-self-center my-4"
+              label="Loading available Conda Python versions..."
+              classNames={{circle2: 'border-b-[#ffe66e]', circle1: 'border-b-[#ffe66e] '}}
+            />
+          ) : (
+            <List
+              renderItem={item => (
+                <List.Item
+                  actions={[
+                    <Popover key={'install_conda_python'}>
+                      <PopoverTrigger>
+                        <Button size="sm" variant="flat">
+                          Install
                         </Button>
-                      </div>
-                    </PopoverContent>
-                  </Popover>,
-                ]}
-                className="hover:bg-foreground-100 transition-colors duration-150">
-                <span>{item}</span>
-              </List.Item>
-            )}
-            className="overflow-hidden"
-            dataSource={searchVersions}
-            bordered
-          />
-        )}
-      </OverlayScrollbarsComponent>
+                      </PopoverTrigger>
+                      <PopoverContent>
+                        <div className="px-2 py-1 space-y-2 text-center">
+                          <Input
+                            onKeyUp={(event: any) => {
+                              if (event.key === 'Enter') {
+                                installPython(item);
+                              }
+                            }}
+                            size="sm"
+                            value={envName}
+                            onValueChange={setEnvName}
+                            placeholder="Enter Conda environment name..."
+                          />
+                          <Button size="sm" onPress={() => installPython(item)} fullWidth>
+                            Install v{item}
+                          </Button>
+                        </div>
+                      </PopoverContent>
+                    </Popover>,
+                  ]}
+                  className="hover:bg-foreground-100 transition-colors duration-150">
+                  <span>{item}</span>
+                </List.Item>
+              )}
+              className="overflow-hidden"
+              dataSource={searchVersions}
+              bordered
+            />
+          )}
+        </OverlayScrollbarsComponent>
+      )}
     </>
   );
 }
