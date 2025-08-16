b5c26fc0a46b0a1a0218776ac35e4dbb98e9a10b
kindabrazy <kindofbrazy@gmail.com>
2025-08-16 22:24:05 +0330

    feat(installer): enhance ModuleStepManager to support additional Python modules

--- src/renderer/ListenForEvents.ts
+++ src/renderer/ListenForEvents.ts
@@ -1,11 +1,14 @@
 import {ExtensionRendererApi} from '../../../src/renderer/src/App/Extensions/ExtensionTypes_Renderer_Api';
+import {ModulesThatSupportPython} from '../cross/CrossExtConstants';
 import {getStep} from './components/Modules/ModuleStepManager';
 import pIpc from './PIpc';
 
 export default function listenForEvents(lynxAPI: ExtensionRendererApi) {
   lynxAPI.events.on('card_install_addStep', ({id, addStep}) => {
-    const {index, title, content} = getStep(id);
-    addStep(index, title, content);
+    if (ModulesThatSupportPython.includes(id)) {
+      const {index, title, content} = getStep(id);
+      addStep(index, title, content);
+    }
   });
 
   lynxAPI.events_ipc.on('storage_utils_add_installed_card', ({cardData}) => {
--- src/renderer/components/Modules/ModuleStepManager.ts
+++ src/renderer/components/Modules/ModuleStepManager.ts
@@ -1,27 +1,44 @@
-import {AvailableModules} from '../../../cross/CrossExtConstants';
+import {AvailableModules, ModulesThatSupportPython} from '../../../cross/CrossExtConstants';
 import {PythonVenvSelectItem} from '../../../cross/CrossExtTypes';
 import pIpc from '../../PIpc';
 import {Installer_PythonSelector} from './Installer_PythonSelector';
 
 function addAssociate(id: string, item: PythonVenvSelectItem) {
-  if (id === AvailableModules.comfyui) {
+  if (ModulesThatSupportPython.includes(id)) {
     pIpc.addAssociate({id, dir: item.dir, type: item.type, condaName: item.condaName});
   }
 }
 
 export const getStep = (id: string) => {
+  let index = 1;
+
   switch (id) {
+    case AvailableModules.sdForge:
     case AvailableModules.comfyui:
-      return {
-        index: 1,
-        title: 'Python',
-        content: Installer_PythonSelector(id, addAssociate),
-      };
+    case AvailableModules.comfyuiZluda:
+    case AvailableModules.sdNext:
+    case AvailableModules.a1:
+    case AvailableModules.sdAmd:
+    case AvailableModules.sdForgeAmd:
+    case AvailableModules.onetrainer:
+    case AvailableModules.kohya:
+    case AvailableModules.sdUiux:
+    case AvailableModules.tg:
+    case AvailableModules.lollms:
+    case AvailableModules.ag:
+    case AvailableModules.alltalk:
+      index = 2;
+      break;
+    case AvailableModules.invoke:
+    case AvailableModules.openWebui:
     default:
-      return {
-        index: 1,
-        title: 'Python',
-        content: Installer_PythonSelector(id, addAssociate),
-      };
+      index = 1;
+      break;
   }
+
+  return {
+    index,
+    title: 'Python',
+    content: Installer_PythonSelector(id, addAssociate),
+  };
 };
