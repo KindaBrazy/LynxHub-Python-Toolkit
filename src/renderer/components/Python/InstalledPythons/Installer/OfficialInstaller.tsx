f66dd8b1b91d360f3bd3265e9101780601d96532
kindabrazy <kindofbrazy@gmail.com>
2025-11-06 12:27:22 +0330

    refactor(renderer): replace localStorage with IPC storage for caching

--- src/renderer/components/Python/InstalledPythons/Installer/OfficialInstaller.tsx
+++ src/renderer/components/Python/InstalledPythons/Installer/OfficialInstaller.tsx
@@ -11,8 +11,6 @@ import {DlProgressOfficial, PythonVersion} from '../../../../../cross/CrossExtTy
 import pIpc from '../../../../PIpc';
 import {Warn_Icon} from '../../../SvgIcons';
 
-const CACHE_KEY = 'available-pythons-list';
-
 type Props = {
   isOpen: boolean;
   closeModal: () => void;
@@ -39,43 +37,45 @@ export default function InstallerOfficial({refresh, installed, closeModal, isOpe
 
   const fetchPythonList = (refresh: boolean) => {
     setLoadingList(true);
-    const cachedList = localStorage.getItem(CACHE_KEY);
-    if (!refresh && !isNil(cachedList)) {
-      setVersions((JSON.parse(cachedList) as PythonVersion[]).filter(item => !installed.includes(item.version)));
-      setLoadingList(false);
-    } else {
-      pIpc
-        .getAvailableOfficial()
-        .then((result: PythonVersion[]) => {
-          localStorage.setItem(CACHE_KEY, JSON.stringify(result));
-          const filteredVersions = result.filter(item => !installed.includes(item.version));
-          setVersions(filteredVersions);
-          setLoadingList(false);
-          setErrorLoadingVersion(undefined);
-        })
-        .catch(e => {
-          if (e.message && isString(e.message)) {
-            if (e.message.toLowerCase().includes('deadsnakes')) {
-              setErrorLoadingVersion({
-                title: 'Deadsnakes PPA Missing',
-                description:
-                  'The application tried to add the Deadsnakes PPA but failed. ' +
-                  'Please add it manually by running the following commands :' +
-                  ' `sudo add-apt-repository -y ppa:deadsnakes/ppa` and then `sudo apt update`.',
-              });
+
+    pIpc.storage.getAvailableOfficial().then(cachedList => {
+      if (!refresh && !isNil(cachedList)) {
+        setVersions(cachedList.filter(item => !installed.includes(item.version)));
+        setLoadingList(false);
+      } else {
+        pIpc
+          .getAvailableOfficial()
+          .then((result: PythonVersion[]) => {
+            pIpc.storage.setAvailableOfficial(result);
+            const filteredVersions = result.filter(item => !installed.includes(item.version));
+            setVersions(filteredVersions);
+            setLoadingList(false);
+            setErrorLoadingVersion(undefined);
+          })
+          .catch(e => {
+            if (e.message && isString(e.message)) {
+              if (e.message.toLowerCase().includes('deadsnakes')) {
+                setErrorLoadingVersion({
+                  title: 'Deadsnakes PPA Missing',
+                  description:
+                    'The application tried to add the Deadsnakes PPA but failed. ' +
+                    'Please add it manually by running the following commands :' +
+                    ' `sudo add-apt-repository -y ppa:deadsnakes/ppa` and then `sudo apt update`.',
+                });
+              } else {
+                setErrorLoadingVersion({title: 'Failed to fetch Python versions', description: e.message});
+              }
             } else {
-              setErrorLoadingVersion({title: 'Failed to fetch Python versions', description: e.message});
+              setErrorLoadingVersion({
+                title: 'Failed to fetch Python versions',
+                description:
+                  'Please check your internet connection and try again.' +
+                  ' If the problem persists, please open an issue on GitHub.',
+              });
             }
-          } else {
-            setErrorLoadingVersion({
-              title: 'Failed to fetch Python versions',
-              description:
-                'Please check your internet connection and try again.' +
-                ' If the problem persists, please open an issue on GitHub.',
-            });
-          }
-        });
-    }
+          });
+      }
+    });
   };
 
   useEffect(() => {
