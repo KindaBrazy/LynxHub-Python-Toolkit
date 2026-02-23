9be24108b03e8794e757945b5d7923cfaa2afd70
KindaBrazy <kindofbrazy@gmail.com>
2026-02-23 17:40:53 +0330

    refactor(extension): update event handling and IPC communication

--- src/renderer/ListenForEvents.ts
+++ src/renderer/ListenForEvents.ts
@@ -1,16 +1,24 @@
+import {storageUtilsChannels} from '@lynx_common/consts/ipcChannels/storage';
 import {ExtensionRendererApi} from '@lynx_common/types/plugins/extensions/api';
 
-// TODO: Update this after new event implemented
+import {ModulesThatSupportPython} from '../cross/CrossExtConstants';
+import {getStep} from './components/Modules/ModuleStepManager';
+import pIpc from './PIpc';
 
-export default function listenForEvents(_lynxAPI: ExtensionRendererApi) {
-  /*lynxAPI.events.on('card_install_addStep', ({id, addStep}) => {
+export default function listenForEvents(lynxAPI: ExtensionRendererApi) {
+  lynxAPI.events.on('card_install_addStep', ({id, addStep}) => {
     if (ModulesThatSupportPython.includes(id)) {
       const {index, title, content} = getStep(id);
       addStep(index, title, content);
     }
   });
 
-  lynxAPI.events_ipc.on(storageUtilsChannels.addInstalledCard, ({cardData}) => {
-    pIpc.findAIVenv(cardData.id, cardData.dir);
-  });*/
+  lynxAPI.ipcEvents.onChannel('before', storageUtilsChannels.addInstalledCard, event => {
+    if (event.method !== 'send') {
+      return;
+    }
+
+    const [cardData] = event.args as [{id: string; dir?: string}];
+    void pIpc.findAIVenv(cardData.id, cardData.dir);
+  });
 }
