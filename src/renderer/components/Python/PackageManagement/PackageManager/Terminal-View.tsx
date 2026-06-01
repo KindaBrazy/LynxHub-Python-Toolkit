52826b2f012febc4f64579744a7c1608a3e5b582
KindaBrazy <kindofbrazy@gmail.com>
2026-06-01 08:55:50 +0330

    refactor(terminal): use direct id prop

--- src/renderer/components/Python/PackageManagement/PackageManager/Terminal-View.tsx
+++ src/renderer/components/Python/PackageManagement/PackageManager/Terminal-View.tsx
@@ -1,8 +1,5 @@
 import XTermCore from '@lynx/components/XTermCore';
-import {useMemo} from 'react';
 
 export default function TerminalView() {
-  const id = useMemo(() => 'python-update', []);
-
-  return <XTermCore id={id} minResizeCols={0} minResizeRows={0} className="overflow-hidden" />;
+  return <XTermCore minResizeCols={0} minResizeRows={0} id="python-update" className="overflow-hidden" />;
 }
