803e3eb00b1b0c1bfb2734b9c713d6894a6e4587
KindaBrazy <kindofbrazy@gmail.com>
2026-07-05 12:37:39 +0330

    refactor(ui): integrate CardsContainer and ScrollShadow in Python Toolkit page layout

--- src/renderer/components/Python/PythonToolkitPage.tsx
+++ src/renderer/components/Python/PythonToolkitPage.tsx
@@ -1,7 +1,9 @@
-import {Key, Tabs} from '@heroui/react';
+import {Key, ScrollShadow, Tabs} from '@heroui/react';
+import CardsContainer from '@lynx/pages/CardsContainer';
 import {useState} from 'react';
 
 import {PythonInstallation} from '../../../cross/CrossExtTypes';
+import {PythonIcon} from '../SvgIcons';
 import InstalledPythons from './InstalledPythons/InstalledPythons';
 import Venv from './VirtualEnvironments/Venv';
 
@@ -12,33 +14,43 @@ export default function PythonToolkitPage() {
   const [currentTab, setCurrentTab] = useState<Key>('installation');
 
   return (
-    <div className="flex flex-col h-full w-full">
-      <div className="pt-5 pb-2 px-4">
-        <Tabs className="w-full" selectedKey={currentTab} onSelectionChange={setCurrentTab}>
-          <Tabs.ListContainer>
-            <Tabs.List aria-label="Options">
-              <Tabs.Tab id="installation">
-                Installations
-                <Tabs.Indicator />
-              </Tabs.Tab>
-              <Tabs.Tab id="venv">
-                Virtual Environments
-                <Tabs.Indicator />
-              </Tabs.Tab>
-            </Tabs.List>
-          </Tabs.ListContainer>
-        </Tabs>
-      </div>
-      <div className="flex-1 overflow-y-auto p-4 scrollbar-hide">
-        <InstalledPythons
-          installedPythons={installedPythons}
-          isLoadingPythons={isLoadingPythons}
-          visible={currentTab === 'installation'}
-          setIsLoadingPythons={setIsLoadingPythons}
-          setInstalledPythons={setInstalledPythons}
-        />
-        <Venv visible={currentTab === 'venv'} installedPythons={installedPythons} isLoadingPythons={isLoadingPythons} />
-      </div>
+    <div className="size-full p-5">
+      <CardsContainer
+        title="Python Toolkit"
+        icon={<PythonIcon className="size-6 mr-2" />}
+        extraClassNames="mr-3 size-full overflow-hidden"
+        subTitle="Manage Python versions, virtual environments, packages, requirements and more.">
+        <div className="pt-5 pb-2 px-4">
+          <Tabs className="w-full" selectedKey={currentTab} onSelectionChange={setCurrentTab}>
+            <Tabs.ListContainer>
+              <Tabs.List aria-label="Options">
+                <Tabs.Tab id="installation">
+                  Installations
+                  <Tabs.Indicator />
+                </Tabs.Tab>
+                <Tabs.Tab id="venv">
+                  Virtual Environments
+                  <Tabs.Indicator />
+                </Tabs.Tab>
+              </Tabs.List>
+            </Tabs.ListContainer>
+          </Tabs>
+        </div>
+        <ScrollShadow className="flex-1 p-4">
+          <InstalledPythons
+            installedPythons={installedPythons}
+            isLoadingPythons={isLoadingPythons}
+            visible={currentTab === 'installation'}
+            setIsLoadingPythons={setIsLoadingPythons}
+            setInstalledPythons={setInstalledPythons}
+          />
+          <Venv
+            visible={currentTab === 'venv'}
+            installedPythons={installedPythons}
+            isLoadingPythons={isLoadingPythons}
+          />
+        </ScrollShadow>
+      </CardsContainer>
     </div>
   );
 }
