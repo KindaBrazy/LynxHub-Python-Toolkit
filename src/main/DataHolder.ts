1535246c7aff1bc64d3fe1d761dd33bbc524b0ab
KindaBrazy <kindofbrazy@gmail.com>
2026-07-26 18:04:54 +0330

    fix(extension): update StorageManager import paths to use storageSqlite

--- src/main/DataHolder.ts
+++ src/main/DataHolder.ts
@@ -1,7 +1,7 @@
 import type Pty from 'node-pty';
 
 import type ElectronAppManager from '../../../src/main/mainWindow';
-import type StorageManager from '../../../src/main/storage/storageOperations';
+import type StorageManager from '../../../src/main/storageSqlite/storageOperations';
 import {DefaultLynxPython_StorageID} from '../cross/CrossExtConstants';
 
 let storageManager: StorageManager | undefined = undefined;
